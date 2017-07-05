20c12727d450ac54dbc96cc59f0afece25a49626
Brian DuVerneay <duverneay@gmail.com>
2017-07-04 19:25:04 -0500

    first commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by BRIAN DUVERNEAY on 7/4/17.
+//  Copyright © 2017 BRIAN DUVERNEAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
