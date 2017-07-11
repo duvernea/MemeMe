f2a0e1507cf015d900f7587a657659530f6eda00
Brian DuVerneay <duverneay@gmail.com>
2017-07-10 20:33:35 -0500

    feat: Create MemeCollectionVC and custom collection cell

---
+++ MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by BRIAN DUVERNEAY on 7/10/17.
+//  Copyright © 2017 BRIAN DUVERNEAY. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var topTextView: UITextView!
+    @IBOutlet var memeImageView: UIImageView!
+    @IBOutlet var bottomTextView: UITextView!
+}
